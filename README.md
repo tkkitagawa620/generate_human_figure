dd0f78418c4cabfbe2946e1271fe20d4af5c0df0
tkkitagawa620 <tkkitagawa620.dev@gmail.com>
2022-03-18 22:25:28 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,25 +1,12 @@
-# VanishingPoint
+# Human Figure Genrator
 
-This project aims to find the vanishing point of the images.
-Code is available in Python and C++.
+This repository consists of the following modules.
 
-You can find complete explaination of the logic and documentation of the code [here](https://www.scribd.com/document/510883652/Vanishing-Point).
-
+- Vanishing point detection
+- Area detection
+- Scale detection
+- Posture determinator
 
 ### Installation
 
-Clone this repository and follow the steps below to run the code.
-
-##### Python
-* Install the following dependencies.
-    * `opencv-python`
-    * `numpy`
-* Set the path of the folder containing input images in the file `main.py` at line 141.
-* Run the code using terminal
-    * Navigate to the cwd.
-    * Run: `$ python main.py`
-    
-#### C++
-* Make sure you have OpenCV binaries installed.
-* Set the path of the folder containing input images in the file `main.cpp` at line 199.
-* Run the file `main.cpp`. For CMake users, `CMakeLists.txt` is also created.
+Clone this repository and install packages listed in `requirements.txt`.
---
+++ detectron_test.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from detectron2.data import MetadataCatalog
+from detectron2.engine.defaults import DefaultPredictor
+from detectron2.data.detection_utils import read_image
+from detectron2 import model_zoo
+from detectron2.model_zoo.model_zoo import _ModelZooUrls
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+
+img_path = "input.png"
+Path("out").mkdir(exist_ok=True)
+img = read_image(img_path, format="BGR")
+
+for i, config_path in enumerate(_ModelZooUrls.CONFIG_PATH_TO_URL_SUFFIX.keys()):
+    # rpnとfast_rcnnは可視化対応していないので飛ばす
+    if "rpn" in config_path or "fast_rcnn" in config_path:
+        continue
+
+    # config設定
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(config_path + ".yaml"))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_path + ".yaml")
+    cfg.MODEL.DEVICE = 'cpu'
+    score_thresh = 0.7
+    cfg.MODEL.RETINANET.SCORE_THRESH_TEST = score_thresh
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = score_thresh
+    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = score_thresh
+    cfg.freeze()
+
+    # 検出＆可視化
+    predictor = DefaultPredictor(cfg)
+    visualizer = Visualizer(
+        img[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2
+    )
+    predictions = predictor(img)
+    if "panoptic_seg" in predictions:
+        panoptic_seg, segments_info = predictions["panoptic_seg"]
+        vis_output = visualizer.draw_panoptic_seg_predictions(
+            panoptic_seg.to("cpu"), segments_info
+        )
+    else:
+        if "sem_seg" in predictions:
+            vis_output = visualizer.draw_sem_seg(
+                predictions["sem_seg"].argmax(dim=0).to("cpu")
+            )
+        if "instances" in predictions:
+            instances = predictions["instances"].to("cpu")
+            vis_output = visualizer.draw_instance_predictions(predictions=instances)
+
+    # ファイル出力
+    dataset_name, algorithm = config_path.split("/")
+    algorithm = algorithm.split(".")[0]
+    vis_output.save(f"out/{i:02d}-{dataset_name}-{algorithm}.jpg")
